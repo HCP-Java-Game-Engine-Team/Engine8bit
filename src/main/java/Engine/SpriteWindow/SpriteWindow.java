607db51256345bdd443f51038c63b561bff782bb
ncrouch1 <bjoonho@outlook.com>
2022-08-14 22:21:03 -0700

    Commenting

--- src/main/java/Engine/SpriteWindow/SpriteWindow.java
+++ src/main/java/Engine/SpriteWindow/SpriteWindow.java
@@ -11,21 +11,38 @@ import javafx.stage.Stage;
 
 import java.util.InputMismatchException;
 
+/*
+    Step: Program a sprite editing platform
+ */
 public class SpriteWindow extends Application {
+
+    // private attribute that contains the structure of the window
     private Group root;
 
+    // Method needed to open the window
     public static void main(String[] args) {
         launch(args);
     }
 
+
+    // Method that declares the structure of the window
     @Override
     public void start(Stage stage) throws Exception {
+        // Set name of window
         stage.setTitle("Sprite Editor");
+        // Set visibility of window
         stage.show();
+        // create root as a group of nodes
+        root = new Group();
+        // create new scene and add root as node group
         Scene sprScene = new Scene(root);
+        // set the stage scene as the previously created scene
         stage.setScene(sprScene);
 
+
+        // get the canvas # of pixels from the getCanvas method
         int canvasSize = getCanvas();
+        // print out the # of pixels for testing
         System.out.println(canvasSize);
     }
 
